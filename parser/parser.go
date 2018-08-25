088ea8fc9a7a199ffd43e47adedcf13401774509
Bo0km4n <pokopenkatuya@gmail.com>
2018-08-25 15:18:02 +0900

    add return parser

--- parser/parser.go
+++ parser/parser.go
@@ -58,6 +58,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -82,6 +84,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// pp.Println(stmt)
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: セミコロンに到達するまで読み飛ばしている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
 	return stmt
 }
 
