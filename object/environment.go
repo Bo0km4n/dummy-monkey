70265f7333b7817f2e050f5c1786a936cdd8a230
Bo0km4n <pokopenkatuya@gmail.com>
2018-11-17 18:30:25 +0900

    Add call function evaluator

--- object/environment.go
+++ object/environment.go
@@ -2,8 +2,16 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	s := make(map[string]Object)
+	return &Environment{
+		store: s,
+		outer: outer,
+	}
+}
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{
@@ -13,6 +21,9 @@ func NewEnvironment() *Environment {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
