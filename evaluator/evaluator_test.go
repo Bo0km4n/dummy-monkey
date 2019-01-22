d4cd51f99ab14a9e4d83bd448dfceef5d874862e
Bo0km4n <pokopenkatuya@gmail.com>
2019-01-22 16:57:51 +0900

    Add array evaluator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -343,3 +343,20 @@ func TestStringConcatention(t *testing.T) {
 		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
+
+func TestArrayLiterals(t *testing.T) {
+	in := "[1, 2 * 2, 3 + 3]"
+
+	evaluated := testEval(in)
+	result, ok := evaluated.(*object.Array)
+	if !ok {
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(result.Elements) != 3 {
+		t.Fatalf("array has wrong num of elements. got=%d", len(result.Elements))
+	}
+	testIntegerObject(t, result.Elements[0], 1)
+	testIntegerObject(t, result.Elements[1], 4)
+	testIntegerObject(t, result.Elements[2], 6)
+}
